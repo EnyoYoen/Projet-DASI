e24f5e90cec4ba9ace52351093d1113f165d8c30
Yoen Peyrot <ypeyrot@insa-lyon.fr>
2025-04-07 15:16:41 +0200

    Creation des entités et des DAO

--- target/generated-sources/annotations/metier/modele/Etablissement_.java
+++ target/generated-sources/annotations/metier/modele/Etablissement_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import metier.modele.Coordonnees;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-03-31T17:39:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-04-07T15:16:03")
 @StaticMetamodel(Etablissement.class)
 public class Etablissement_ { 
 
--- target/generated-sources/annotations/metier/modele/Intervenant_.java
+++ target/generated-sources/annotations/metier/modele/Intervenant_.java
@@ -4,9 +4,9 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-03-31T17:39:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-04-07T15:16:03")
 @StaticMetamodel(Intervenant.class)
-public class Intervenant_ extends Personne_ {
+public abstract class Intervenant_ extends Personne_ {
 
     public static volatile SingularAttribute<Intervenant, Boolean[]> niveaux;
     public static volatile SingularAttribute<Intervenant, String> numTel;
