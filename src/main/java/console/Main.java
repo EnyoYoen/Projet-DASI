19405c62ed469999e73b0fa80c6f89e19c238b80
EnyoYoen <enyotoyrep@gmail.com>
2025-04-14 17:11:04 +0200

    test

--- src/main/java/console/Main.java
+++ src/main/java/console/Main.java
@@ -19,6 +19,8 @@ public class Main {
 
         JpaUtil.creerFabriquePersistance();
 
+        System.out.println(service.recupererMatiere());
+
         //service.init();
         //testerInscrireEleve();
         Personne personne = service.authentification("pierrelafon1@gmail.com", "mdp");
